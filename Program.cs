709a432472374f39a27a491de556805ceea721ab
dhowe <daniel@rednoise.org>
2017-12-27 00:24:30 +0800

    initial

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Antlr4.Runtime;
+
+namespace pkgtest {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine(new AntlrInputStream("Test"));
+    }
+  }
+}
